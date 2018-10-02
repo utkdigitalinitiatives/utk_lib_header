2a1fba47a9e351953765bea6016a56ced9b6eb8b
Mat Jordan <mat@utk.edu>
2018-10-02 11:34:36 -0400

    menu expand functionality shifts informed by brand adjustments in previous few commits

--- src/Header.js
+++ src/Header.js
@@ -11,7 +11,8 @@ import {Search} from "./components/Search";
 /* assets */
 import './css/utk-lib-header.css';
 import primary_logo from './media/ut-knoxville.svg';
-import shortcut_logo from './media/ut-square.svg';
+import square_logo from './media/ut-square.svg';
+import {Placeholder} from "./components/Placeholder";
 
 /* header component */
 class Header extends Component {
@@ -133,9 +134,10 @@ class Header extends Component {
 
                         <div className="utk-logo-wrapper">
                             <a href="https://www.utk.edu">
-                                <img src={primary_logo} className="utk-logo" alt="University of Tennessee Libraries" />
+                                <img src={primary_logo} className="utk-logo utk-logo-primary" alt="University of Tennessee Libraries" />
+                                <img src={square_logo} className="utk-logo utk-logo-square" alt="University of Tennessee Libraries" />
                             </a>
-                            <a href="https://www.utk.edu" className="utk-logo-unit">Libraries</a>
+                            <a href="https://www.lib.utk.edu" className="utk-logo-unit">Libraries</a>
                         </div>
 
                         <div className="utk-header-actions">
@@ -169,6 +171,22 @@ class Header extends Component {
                             <li><a href="#">Locations</a></li>
                             <li><a href="#">Databases</a></li>
                             <li><a href="#">Services</a></li>
+                            <li className="utk-header-super--menu--options">
+                            <div className="utk-header-actions--item utk-header-actions--resources">
+                                <a onClick={this.toggleResources} className={`utk-menu-trigger${resourcesClass}`}>
+                                    <span className="icon-menu"></span>
+                                    <span className="icon-cancel"></span>
+                                    <em>Menu</em>
+                                </a>
+                            </div>
+
+                            <div className="utk-header-actions--item utk-header-actions--search">
+                                <a onClick={this.toggleSearch} className={searchClass}>
+                                    <span className="icon-search"></span>
+                                    <span className="icon-cancel"></span>
+                                </a>
+                            </div>
+                            </li>
                         </ul>
                     </div>
                 </div>
@@ -176,71 +194,7 @@ class Header extends Component {
                 <Search active={searchClass} showSearch={showSearch} ref="search" />
             </header>
             <div className="utk-body-overlay"></div>
-                <div id="lipsum">
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam euismod augue justo, eu tempus libero condimentum eu. Nulla cursus ut enim in fringilla. Ut eu pulvinar ex. Curabitur pretium massa eleifend ex tempor molestie. Aenean faucibus congue nibh, sit amet tempus augue eleifend eu. Morbi sollicitudin nunc sed imperdiet imperdiet. Fusce tincidunt luctus erat, ut finibus tortor pharetra vel. Praesent vel rutrum sapien. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum varius ex in sem fringilla, a vehicula sem ornare.
-                    </p>
-                    <p>
-                        Vestibulum volutpat cursus elit, id vehicula nunc aliquet nec. Duis vestibulum massa quis nunc mattis, sit amet vulputate quam congue. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam a elit nisi. Vestibulum pretium interdum accumsan. Nulla condimentum magna vitae varius gravida. Donec pellentesque lacinia nulla, eleifend pellentesque nisl viverra vitae. Phasellus aliquam erat et rhoncus consequat. Quisque cursus arcu at massa facilisis, eu tincidunt erat vestibulum. Suspendisse pretium convallis sem eu ullamcorper. Ut justo mauris, vestibulum in sem nec, blandit dictum urna.
-                    </p>
-                    <p>
-                        Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In commodo, purus vitae tempor tristique, massa odio mattis purus, sit amet elementum lectus metus vel diam. Phasellus nec condimentum ex. Praesent erat est, interdum sit amet gravida eu, interdum et nisi. Nulla non turpis vel sapien aliquet elementum et quis sapien. Proin orci purus, molestie et malesuada quis, pharetra nec ligula. Curabitur aliquam velit ut sapien euismod, non euismod mi malesuada. Phasellus quis ipsum a libero tincidunt euismod at eget lacus. Vestibulum laoreet mattis fringilla. Donec lobortis ultrices risus, ac suscipit quam malesuada ac. Nulla facilisi. Etiam hendrerit a turpis eget ultricies. Quisque iaculis dapibus consequat. Nunc pulvinar gravida diam, eget semper sapien placerat non. Phasellus euismod ipsum ut feugiat congue.
-                    </p>
-                    <p>
-                        Etiam dapibus tellus at ultrices tempus. Phasellus sagittis lobortis diam, non interdum nibh interdum ut. Vestibulum sollicitudin in enim at mattis. Nullam quis odio et quam malesuada tincidunt. Suspendisse at tortor et libero eleifend blandit in consectetur metus. Praesent quis elit nibh. Duis quis tortor vel neque fermentum porta.
-                    </p>
-                    <p>
-                        Fusce fringilla dui est, ac pharetra quam euismod vitae. Nulla vehicula magna tortor, id blandit libero mollis ac. Pellentesque convallis lacus eu leo hendrerit, ac sollicitudin lectus blandit. Pellentesque et erat laoreet, mattis est ut, luctus est. Maecenas eget risus lacinia, hendrerit arcu vel, aliquam dolor. Maecenas molestie ullamcorper felis at tincidunt. Suspendisse lacinia lorem id ipsum suscipit, ac tristique turpis malesuada. Curabitur ac ex at orci molestie condimentum. Donec suscipit sagittis faucibus. Praesent eleifend eleifend justo, sed accumsan sem ultricies non. Cras eu interdum lorem, dignissim commodo purus.
-                    </p>
-                    <p>
-                        Vestibulum volutpat cursus elit, id vehicula nunc aliquet nec. Duis vestibulum massa quis nunc mattis, sit amet vulputate quam congue. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam a elit nisi. Vestibulum pretium interdum accumsan. Nulla condimentum magna vitae varius gravida. Donec pellentesque lacinia nulla, eleifend pellentesque nisl viverra vitae. Phasellus aliquam erat et rhoncus consequat. Quisque cursus arcu at massa facilisis, eu tincidunt erat vestibulum. Suspendisse pretium convallis sem eu ullamcorper. Ut justo mauris, vestibulum in sem nec, blandit dictum urna.
-                    </p>
-                    <p>
-                        Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In commodo, purus vitae tempor tristique, massa odio mattis purus, sit amet elementum lectus metus vel diam. Phasellus nec condimentum ex. Praesent erat est, interdum sit amet gravida eu, interdum et nisi. Nulla non turpis vel sapien aliquet elementum et quis sapien. Proin orci purus, molestie et malesuada quis, pharetra nec ligula. Curabitur aliquam velit ut sapien euismod, non euismod mi malesuada. Phasellus quis ipsum a libero tincidunt euismod at eget lacus. Vestibulum laoreet mattis fringilla. Donec lobortis ultrices risus, ac suscipit quam malesuada ac. Nulla facilisi. Etiam hendrerit a turpis eget ultricies. Quisque iaculis dapibus consequat. Nunc pulvinar gravida diam, eget semper sapien placerat non. Phasellus euismod ipsum ut feugiat congue.
-                    </p>
-                    <p>
-                        Etiam dapibus tellus at ultrices tempus. Phasellus sagittis lobortis diam, non interdum nibh interdum ut. Vestibulum sollicitudin in enim at mattis. Nullam quis odio et quam malesuada tincidunt. Suspendisse at tortor et libero eleifend blandit in consectetur metus. Praesent quis elit nibh. Duis quis tortor vel neque fermentum porta.
-                    </p>
-                    <p>
-                        Fusce fringilla dui est, ac pharetra quam euismod vitae. Nulla vehicula magna tortor, id blandit libero mollis ac. Pellentesque convallis lacus eu leo hendrerit, ac sollicitudin lectus blandit. Pellentesque et erat laoreet, mattis est ut, luctus est. Maecenas eget risus lacinia, hendrerit arcu vel, aliquam dolor. Maecenas molestie ullamcorper felis at tincidunt. Suspendisse lacinia lorem id ipsum suscipit, ac tristique turpis malesuada. Curabitur ac ex at orci molestie condimentum. Donec suscipit sagittis faucibus. Praesent eleifend eleifend justo, sed accumsan sem ultricies non. Cras eu interdum lorem, dignissim commodo purus.
-                    </p>
-                    <p>
-                        Vestibulum volutpat cursus elit, id vehicula nunc aliquet nec. Duis vestibulum massa quis nunc mattis, sit amet vulputate quam congue. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam a elit nisi. Vestibulum pretium interdum accumsan. Nulla condimentum magna vitae varius gravida. Donec pellentesque lacinia nulla, eleifend pellentesque nisl viverra vitae. Phasellus aliquam erat et rhoncus consequat. Quisque cursus arcu at massa facilisis, eu tincidunt erat vestibulum. Suspendisse pretium convallis sem eu ullamcorper. Ut justo mauris, vestibulum in sem nec, blandit dictum urna.
-                    </p>
-                    <p>
-                        Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In commodo, purus vitae tempor tristique, massa odio mattis purus, sit amet elementum lectus metus vel diam. Phasellus nec condimentum ex. Praesent erat est, interdum sit amet gravida eu, interdum et nisi. Nulla non turpis vel sapien aliquet elementum et quis sapien. Proin orci purus, molestie et malesuada quis, pharetra nec ligula. Curabitur aliquam velit ut sapien euismod, non euismod mi malesuada. Phasellus quis ipsum a libero tincidunt euismod at eget lacus. Vestibulum laoreet mattis fringilla. Donec lobortis ultrices risus, ac suscipit quam malesuada ac. Nulla facilisi. Etiam hendrerit a turpis eget ultricies. Quisque iaculis dapibus consequat. Nunc pulvinar gravida diam, eget semper sapien placerat non. Phasellus euismod ipsum ut feugiat congue.
-                    </p>
-                    <p>
-                        Etiam dapibus tellus at ultrices tempus. Phasellus sagittis lobortis diam, non interdum nibh interdum ut. Vestibulum sollicitudin in enim at mattis. Nullam quis odio et quam malesuada tincidunt. Suspendisse at tortor et libero eleifend blandit in consectetur metus. Praesent quis elit nibh. Duis quis tortor vel neque fermentum porta.
-                    </p>
-                    <p>
-                        Fusce fringilla dui est, ac pharetra quam euismod vitae. Nulla vehicula magna tortor, id blandit libero mollis ac. Pellentesque convallis lacus eu leo hendrerit, ac sollicitudin lectus blandit. Pellentesque et erat laoreet, mattis est ut, luctus est. Maecenas eget risus lacinia, hendrerit arcu vel, aliquam dolor. Maecenas molestie ullamcorper felis at tincidunt. Suspendisse lacinia lorem id ipsum suscipit, ac tristique turpis malesuada. Curabitur ac ex at orci molestie condimentum. Donec suscipit sagittis faucibus. Praesent eleifend eleifend justo, sed accumsan sem ultricies non. Cras eu interdum lorem, dignissim commodo purus.
-                    </p>
-                    <p>
-                        Vestibulum volutpat cursus elit, id vehicula nunc aliquet nec. Duis vestibulum massa quis nunc mattis, sit amet vulputate quam congue. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam a elit nisi. Vestibulum pretium interdum accumsan. Nulla condimentum magna vitae varius gravida. Donec pellentesque lacinia nulla, eleifend pellentesque nisl viverra vitae. Phasellus aliquam erat et rhoncus consequat. Quisque cursus arcu at massa facilisis, eu tincidunt erat vestibulum. Suspendisse pretium convallis sem eu ullamcorper. Ut justo mauris, vestibulum in sem nec, blandit dictum urna.
-                    </p>
-                    <p>
-                        Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In commodo, purus vitae tempor tristique, massa odio mattis purus, sit amet elementum lectus metus vel diam. Phasellus nec condimentum ex. Praesent erat est, interdum sit amet gravida eu, interdum et nisi. Nulla non turpis vel sapien aliquet elementum et quis sapien. Proin orci purus, molestie et malesuada quis, pharetra nec ligula. Curabitur aliquam velit ut sapien euismod, non euismod mi malesuada. Phasellus quis ipsum a libero tincidunt euismod at eget lacus. Vestibulum laoreet mattis fringilla. Donec lobortis ultrices risus, ac suscipit quam malesuada ac. Nulla facilisi. Etiam hendrerit a turpis eget ultricies. Quisque iaculis dapibus consequat. Nunc pulvinar gravida diam, eget semper sapien placerat non. Phasellus euismod ipsum ut feugiat congue.
-                    </p>
-                    <p>
-                        Etiam dapibus tellus at ultrices tempus. Phasellus sagittis lobortis diam, non interdum nibh interdum ut. Vestibulum sollicitudin in enim at mattis. Nullam quis odio et quam malesuada tincidunt. Suspendisse at tortor et libero eleifend blandit in consectetur metus. Praesent quis elit nibh. Duis quis tortor vel neque fermentum porta.
-                    </p>
-                    <p>
-                        Fusce fringilla dui est, ac pharetra quam euismod vitae. Nulla vehicula magna tortor, id blandit libero mollis ac. Pellentesque convallis lacus eu leo hendrerit, ac sollicitudin lectus blandit. Pellentesque et erat laoreet, mattis est ut, luctus est. Maecenas eget risus lacinia, hendrerit arcu vel, aliquam dolor. Maecenas molestie ullamcorper felis at tincidunt. Suspendisse lacinia lorem id ipsum suscipit, ac tristique turpis malesuada. Curabitur ac ex at orci molestie condimentum. Donec suscipit sagittis faucibus. Praesent eleifend eleifend justo, sed accumsan sem ultricies non. Cras eu interdum lorem, dignissim commodo purus.
-                    </p>
-                    <p>
-                        Vestibulum volutpat cursus elit, id vehicula nunc aliquet nec. Duis vestibulum massa quis nunc mattis, sit amet vulputate quam congue. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam a elit nisi. Vestibulum pretium interdum accumsan. Nulla condimentum magna vitae varius gravida. Donec pellentesque lacinia nulla, eleifend pellentesque nisl viverra vitae. Phasellus aliquam erat et rhoncus consequat. Quisque cursus arcu at massa facilisis, eu tincidunt erat vestibulum. Suspendisse pretium convallis sem eu ullamcorper. Ut justo mauris, vestibulum in sem nec, blandit dictum urna.
-                    </p>
-                    <p>
-                        Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In commodo, purus vitae tempor tristique, massa odio mattis purus, sit amet elementum lectus metus vel diam. Phasellus nec condimentum ex. Praesent erat est, interdum sit amet gravida eu, interdum et nisi. Nulla non turpis vel sapien aliquet elementum et quis sapien. Proin orci purus, molestie et malesuada quis, pharetra nec ligula. Curabitur aliquam velit ut sapien euismod, non euismod mi malesuada. Phasellus quis ipsum a libero tincidunt euismod at eget lacus. Vestibulum laoreet mattis fringilla. Donec lobortis ultrices risus, ac suscipit quam malesuada ac. Nulla facilisi. Etiam hendrerit a turpis eget ultricies. Quisque iaculis dapibus consequat. Nunc pulvinar gravida diam, eget semper sapien placerat non. Phasellus euismod ipsum ut feugiat congue.
-                    </p>
-                    <p>
-                        Etiam dapibus tellus at ultrices tempus. Phasellus sagittis lobortis diam, non interdum nibh interdum ut. Vestibulum sollicitudin in enim at mattis. Nullam quis odio et quam malesuada tincidunt. Suspendisse at tortor et libero eleifend blandit in consectetur metus. Praesent quis elit nibh. Duis quis tortor vel neque fermentum porta.
-                    </p>
-                    <p>
-                        Fusce fringilla dui est, ac pharetra quam euismod vitae. Nulla vehicula magna tortor, id blandit libero mollis ac. Pellentesque convallis lacus eu leo hendrerit, ac sollicitudin lectus blandit. Pellentesque et erat laoreet, mattis est ut, luctus est. Maecenas eget risus lacinia, hendrerit arcu vel, aliquam dolor. Maecenas molestie ullamcorper felis at tincidunt. Suspendisse lacinia lorem id ipsum suscipit, ac tristique turpis malesuada. Curabitur ac ex at orci molestie condimentum. Donec suscipit sagittis faucibus. Praesent eleifend eleifend justo, sed accumsan sem ultricies non. Cras eu interdum lorem, dignissim commodo purus.
-                    </p>
-                </div>
+            <Placeholder/>
             </div>
         );
     }
---
+++ src/components/Placeholder.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+export class Placeholder extends Component {
+
+    render () {
+        return (
+            <div id="placeholder">
+
+            </div>
+        )
+    }
+
+}
