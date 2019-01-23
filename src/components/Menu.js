b35a1f0953493cefcca85ec70dc1e7f7384b26fb
Mat Jordan <mat@utk.edu>
2019-01-23 09:59:26 -0500

    Align options bar in expanded footer to align to container.

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -114,23 +114,25 @@ export class Menu extends Component {
                         <Hours/>
                     </div>
                     <div className='utk-menu-options'>
-                        <div className='utk-menu-help'>
-                            <a className="utk-menu-help--item utk-menu-help--help-me">
-                                <h4>Quick Help</h4>
-                                <div className="utk-menu-help--item--icon">
-                                    <span className="icon-chat"></span>
-                                </div>
-                            </a>
-                            <a className="utk-menu-help--item utk-menu-help--chat">
-                                <h4>Chat</h4>
-                                <div className="utk-menu-help--item--icon">
-                                    <span className="icon-chat"></span>
-                                </div>
+                        <div className='container'>
+                            <div className='utk-menu-help'>
+                                <a className="utk-menu-help--item utk-menu-help--help-me">
+                                    <h4>Quick Help</h4>
+                                    <div className="utk-menu-help--item--icon">
+                                        <span className="icon-chat"></span>
+                                    </div>
+                                </a>
+                                <a className="utk-menu-help--item utk-menu-help--chat">
+                                    <h4>Chat</h4>
+                                    <div className="utk-menu-help--item--icon">
+                                        <span className="icon-chat"></span>
+                                    </div>
+                                </a>
+                            </div>
+                            <a className="utk-resources-close">
+                                <span className="icon-cancel"></span>
                             </a>
                         </div>
-                        <a className="utk-resources-close">
-                            <span className="icon-cancel"></span>
-                        </a>
                     </div>
                 </div>
             </div>
