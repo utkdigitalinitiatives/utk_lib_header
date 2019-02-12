053a0280392794eccb5d5563bd15a0aed739e845
Mat Jordan <mat@utk.edu>
2019-02-12 15:45:08 -0500

    Add digital collection components.

---
+++ src/components/custom/DigitalFooter.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+export class DigitalBody extends Component {
+
+    render () {
+        return (
+            <div>
+                <h1>Digital Collections</h1>
+            </div>
+        )
+    }
+
+}
