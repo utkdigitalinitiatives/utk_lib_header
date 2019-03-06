96f3907faa057305f089439db9fcebcefacd0866
Mat Jordan <mat@utk.edu>
2019-03-06 13:31:06 -0500

    Add remote accessible images.

--- src/components/custom/data/Collections.js
+++ src/components/custom/data/Collections.js
@@ -1,91 +1,91 @@
 const collections = [
     {
         'title': 'Nineteenth and Early Twentieth Century Images of Egypt',
-        'src': 'http://assets.test/dc/egypt.jpg',
+        'src': 'https://www-dev.lib.utk.edu/dc/assets/egypt.jpg',
         'group': 1,
         'featured': 0
     },
     {
         'title': 'Postcards from the Great Smoky Mountains',
-        'src': 'http://assets.test/dc/gsmrc.jpg',
+        'src': 'https://www-dev.lib.utk.edu/dc/assets/gsmrc.jpg',
         'group': 3,
         'featured': 0
     },
     {
         'title': 'Photographs from the Life and Career of Howard Baker',
-        'src': 'http://assets.test/dc/howard2.jpg',
+        'src': 'https://www-dev.lib.utk.edu/dc/assets/howard2.jpg',
         'group': 3,
         'featured': 0
     },
     {
         'title': 'Knoxville Gardens Slides',
-        'src': 'http://assets.test/dc/knoxville-garden.jpg',
+        'src': 'https://www-dev.lib.utk.edu/dc/assets/knoxville-garden.jpg',
         'group': 4,
         'featured': 1
     },
     {
         'title': 'Sanborn Fire Insurance Maps Collection',
-        'src': 'http://assets.test/dc/sanborn.jpg',
+        'src': 'https://www-dev.lib.utk.edu/dc/assets/sanborn.jpg',
         'group': 1,
         'featured': 0
     },
     {
         'title': 'Virginia P. Moore Collection ',
-        'src': 'http://assets.test/dc/sealing.jpg',
+        'src': 'https://www-dev.lib.utk.edu/dc/assets/sealing.jpg',
         'group': 1,
         'featured': 0
     },
     {
         'title': 'The Volunteer Yearbooks ',
-        'src': 'http://assets.test/dc/tn-yearbook.jpg',
+        'src': 'https://www-dev.lib.utk.edu/dc/assets/tn-yearbook.jpg',
         'group': 3,
         'featured': 0
     },
     {
         'title': 'Anna Catherine Wiley Sketches',
-        'src': 'http://assets.test/dc/wiley.jpg',
+        'src': 'https://www-dev.lib.utk.edu/dc/assets/wiley.jpg',
         'group': 4,
         'featured': 0
     },
     {
         'title': 'Druid: A Humanities Magazine',
-        'src': 'http://assets.test/dc/druid.jpg',
+        'src': 'https://www-dev.lib.utk.edu/dc/assets/druid.jpg',
         'group': 1,
         'featured': 0
     },
     {
         'title': 'Fifty Years in Cades Cove',
-        'src': 'http://assets.test/dc/dun-oliver.jpg',
+        'src': 'https://www-dev.lib.utk.edu/dc/assets/dun-oliver.jpg',
         'group': 2,
         'featured': 0
     },
     {
         'title': 'William Cox Cochran Photographic Collection',
-        'src': 'http://assets.test/dc/cochran.jpg',
+        'src': 'https://www-dev.lib.utk.edu/dc/assets/cochran.jpg',
         'group': 3,
         'featured': 0
     },
     {
         'title': 'Albert "Dutch" Roth Photograph Collection',
-        'src': 'http://assets.test/dc/dutch-roth.jpg',
+        'src': 'https://www-dev.lib.utk.edu/dc/assets/dutch-roth.jpg',
         'group': 4,
         'featured': 0
     },
     {
         'title': 'Digital Civil War Collection',
-        'src': 'http://assets.test/dc/civil-war.jpg',
+        'src': 'https://www-dev.lib.utk.edu/dc/assets/civil-war.jpg',
         'group': 4,
         'featured': 0
     },
     {
         'title': 'David Van Vactor Music Collection ',
-        'src': 'http://assets.test/dc/vanvactor.jpg',
+        'src': 'https://www-dev.lib.utk.edu/dc/assets/vanvactor.jpg',
         'group': 4,
         'featured': 1
     },
     {
         'title': 'World War II Oral Histories',
-        'src': 'http://assets.test/dc/ww2.jpg',
+        'src': 'https://www-dev.lib.utk.edu/dc/assets/ww2.jpg',
         'group': 4,
         'featured': 0
     }
