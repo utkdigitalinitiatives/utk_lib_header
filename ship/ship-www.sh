090f1a8628304684d2346981fd9be0fc6f917e76
Mat Jordan <mat@utk.edu>
2019-05-28 12:51:51 -0400

    Update to better handle shipping media.

--- ship/ship-www.sh
+++ ship/ship-www.sh
@@ -1,7 +1,10 @@
 echo 'Shipping React Build to...';
 echo '~/Sites/dev/www/wp-content/themes/utk_lib_wp_theme/resources/assets/react/header';
 
-rsync -av --delete dist ~/Sites/dev/www/wp-content/themes/utk_lib_wp_theme/resources/assets/react/header
+#"build-www-set-absolutes-js": "replace 'static[/]' 'wp-content/themes/utk_lib_wp_theme/resources/assets/react/header/' build/static/js/*.js",
+#"build-www-set-absolutes-css": "replace '[/]static[/]' '/wp-content/themes/utk_lib_wp_theme/resources/assets/react/header/' build/static/css/*.css",
+
+rsync -av --delete dist/ ~/Sites/dev/www/wp-content/themes/utk_lib_wp_theme/resources/assets/react/header
 
 cd ~/Sites/dev/www/wp-content/themes/utk_lib_wp_theme/;
 yarn build;
