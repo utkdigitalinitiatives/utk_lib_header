bfa7dccad3302fb9fccf7e7bca8bc4159c57451b
Mat Jordan <mat@utk.edu>
2019-11-18 09:23:41 -0500

    Refine footer componentry.

--- src/Footer.js
+++ src/Footer.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
-import {FooterSections} from "./components/FooterSections";
+import {FooterLibraries} from "./components/FooterLibraries";
+import {FooterUniversity} from "./components/FooterUniversity";
+import {FooterSystem} from "./components/FooterSystem";
+import Globals from "./components/Globals";
 
 /* header component */
 class Footer extends Component {
@@ -8,14 +11,54 @@ class Footer extends Component {
         super(props);
 
         this.state = {
+            menuHeader : {
+                about : {},
+                drawer : {},
+                help : {},
+                services_primary : {},
+                services_speciality : {}
+            },
         };
-    };
+    }
+
+    componentDidMount() {
+
+        const sessionMenu = 'utk_lib_header_menu';
+
+        if (sessionStorage.getItem(sessionMenu) === null) {
+
+            fetch(Globals.URL + ENDPOINT + ROUTE, {
+                headers : {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then(data => {
+
+                    sessionStorage.setItem(sessionMenu, JSON.stringify(data));
+                    this.setState({menuHeader: data})
+
+                })
+                .catch(err => console.error(this.props.url, err.toString()));
+
+        } else {
+
+            const sessionData = sessionStorage.getItem(sessionMenu);
+            this.setState({menuHeader: JSON.parse(sessionData)});
+
+        }
+
+        return null
+    }
 
     render() {
 
         return (
             <React.Fragment>
-                <FooterSections />
+                <FooterLibraries main={this.state.menuHeader.drawer} help={this.state.menuHeader.help} />
+                {/*<FooterUniversity university={} />*/}
+                {/*<FooterSystem system={} />*/}
                 <div className="utk-lib-triggers">
                     <span id="utk-trigger-header"></span>
                 </div>
