ea6d15a9540e7e41aa0e7b9035061772aecfe0f7
Mat Jordan <mat@utk.edu>
2020-07-13 22:41:16 -0400

    Simplify primary menu presentation.

--- ship/ship-www.sh
+++ ship/ship-www.sh
@@ -1,10 +1,10 @@
 echo 'Shipping React Build to...';
-echo '~/Sites/dev/www/wp-content/themes/utk_lib_wp_theme/resources/assets/react/header';
+echo '~/Sites/dev/utk-libraries/wp-content/themes/utk_lib_wp_theme/resources/assets/react/header';
 
-rsync -av --delete dist/ ~/Sites/dev/www/wp-content/themes/utk_lib_wp_theme/resources/assets/react/header
+rsync -av --delete dist/ ~/Sites/dev/utk-libraries/wp-content/themes/utk_lib_wp_theme/resources/assets/react/header
 
-cd ~/Sites/dev/www/wp-content/themes/utk_lib_wp_theme/;
+cd ~/Sites/dev/utk-libraries/wp-content/themes/utk_lib_wp_theme/;
 yarn build;
-cd ~/Sites/react/utk_lib_header;
+cd ~/Sites/dev/react/utk_lib_header;
 
 echo 'Shipped Complete';
