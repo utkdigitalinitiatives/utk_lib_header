56f8b008c08ac40b47a4d59aa262b01569e464cb
Mat Jordan <mat@utk.edu>
2020-07-14 22:38:27 -0400

    Wrap volumes section mock.

--- src/components/Volumes.js
+++ src/components/Volumes.js
@@ -12,6 +12,8 @@ const ENDPOINT = 'assets/wp-json/libmenu';
 const ROUTE = '/header';
 
 import fall20020 from '../media/fall2020.jpg';
+import extra1 from '../media/fall2020-extra1.jpg';
+import extra2 from '../media/fall2020-extra2.jpg';
 
 
 export class Volumes extends Component {
@@ -29,15 +31,20 @@ export class Volumes extends Component {
                 <a className="utk-volumes--feature">
                     <div>
                         <strong>Donec dictum turpis nisi vel  fermentum tempus</strong>
-                        <em>Etiam lobortis exo arcu semper imperdiet, faucibus sed ad amet</em>
+                        <em>Etiam faucibus exo arcu semper imperdiet, faucibus sed ad amet</em>
                         <span>Vitae Mauris</span>
                     </div>
                     <img src={fall20020} />
                 </a>
-                <div>
-                </div>
-                <div>
-                    this
+                <div className="utk-volumes--extras">
+                    <a className="utk-volumes--extras--item">
+                        <img src={extra1} />
+                        <span>Pellentesque luctus sit amet maximus lobortis</span>
+                    </a>
+                    <a className="utk-volumes--extras--item">
+                        <img src={extra2} />
+                        <span>Pretium accumsan ante pellentesque cras placerat</span>
+                    </a>
                 </div>
             </div>
         )
